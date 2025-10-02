8734502dd77e578b9819bfbd051f66a1babae148
zarchimyintaung2000 <141807383+zarchimyintaung@users.noreply.github.com>
2025-10-02 13:34:40 +0630

    Update ChMessage.php

--- src/Models/ChMessage.php
+++ src/Models/ChMessage.php
@@ -8,4 +8,19 @@ use Chatify\Traits\UUID;
 class ChMessage extends Model
 {
     use UUID;
+
+    protected $fillable = [
+        'type',
+        'from_id',
+        'to_id',
+        'body',
+        'sent_by',
+        'product_id',
+        'attachment',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
